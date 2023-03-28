d7d0e861feabd92437fbd025760786112d86d55f
Hector Gutierrez <hectorgutierrez89@hotmail.com>
2023-03-28 16:01:35 -0600

    Creacion de Proyecto

---
+++ MagicVilla/Controllers/Villa.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Villa : ControllerBase
+    {
+    }
+}
